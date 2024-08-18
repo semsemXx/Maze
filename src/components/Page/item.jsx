fff55631cdc968b70dcc1265e3b09b1f590945cd
semsemXx <guidarawassim98@gmail.com>
2024-08-18 01:25:34 +0100

    Update item.jsx

--- src/components/Page/item.jsx
+++ src/components/Page/item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from '@/css/item.module.css'; 
 import CadreImage from '@/image/cadre.png';
+import Shirt from '@/image/shirt.png'
 export default function ImageComponent() {
   return (
     <div className={classes.imageContainer}>
